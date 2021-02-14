ad982a7047594f2e09d5b357ecbc48efd138fb53
Олег Есиков <esikov.oleg@mail.ru>
2021-02-14 12:14:02 +0300

    add-file

---
+++ c/test_search.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "search.h"
+
+int updateCriticalNumber(int value)
+{
+    if (value == 0)
+    {
+        int temp = 20;
+        value = func(temp) + 45 - 21;
+        return value;
+    }
+    if (value == -10)
+    {
+        return 10;
+    }
+    if (value == 10)
+    {
+        return func(value);
+    }
+}
